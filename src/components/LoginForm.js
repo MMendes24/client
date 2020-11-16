78e240b68d975870833ddcf5e3a1a52f6349769a
MMendes24 <mmbusiness1@protonmail.com>
2020-11-16 12:59:01 -0600

    User can now login

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,42 @@
 import React from "react"
-import { Formik } from "formik"
+import axios from "axios"
+import { Formik, Form, Field } from "formik"
 
 const LoginForm = () => {
+
+    const userLogin = (values) => {
+        axios.post("https://campaign-journal-api.herokuapp.com/api/users/login", values)
+            .then(res => {
+                console.log(res)
+                localStorage.setItem('token', res.data.token);
+            })
+            .catch(err => {
+                console.log("are you serious?")
+            })
+    }
+
     return (
         <div>
-
+            <h2>Login</h2>
+            <Formik
+                initialValues={{ username: '', password: '' }}
+                onSubmit={(values, { setSubmitting }) => {
+                    setTimeout(() => {
+                        userLogin(values)
+                        setSubmitting(false);
+                    }, 400);
+                }}
+            >
+                {({ isSubmitting }) => (
+                    <Form>
+                        <Field type="username" name="username" />
+                        <Field type="password" name="password" />
+                        <button type="submit" disabled={isSubmitting}>
+                            Submit
+                        </button>
+                    </Form>
+                )}
+            </Formik>
         </div>
     )
 }

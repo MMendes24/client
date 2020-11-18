2cd86ab6347f288beab9e2f188646afd8f56b946
MMendes24 <mmbusiness1@protonmail.com>
2020-11-18 15:37:11 -0600

    Validation added to CountryAdd form.

--- src/components/CampaignEdit.js
+++ src/components/CampaignEdit.js
@@ -75,14 +75,18 @@ const CampaignEdit = () => {
                 <Form>
                     <label>Campaign Title:</label>
                     <Field type="text" name="name" />
+
                     {errors.name && touched.name ? (
                         <div className="error">{errors.name}</div>
                     ) : null}
+
                     <label>Campaign Description:</label>
                     <Field type="text" name="description" />
+
                     {errors.description && touched.description ? (
                         <div className="error">{errors.description}</div>
                     ) : null}
+                    
                     <button type="submit" disabled={isSubmitting}>
                         Submit
                     </button>

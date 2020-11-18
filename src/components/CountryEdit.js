482620b0bd605f86354e5437d1f9c77cedabebe4
MMendes24 <mmbusiness1@protonmail.com>
2020-11-18 15:39:05 -0600

    Validation added to CountryEdit form.

--- src/components/CountryEdit.js
+++ src/components/CountryEdit.js
@@ -2,6 +2,22 @@ import React, { useEffect, useState } from "react"
 import axiosAuth from "../utils/axiosAuth"
 import { useHistory, useParams } from "react-router-dom"
 import { Formik, Field, Form } from "formik"
+import * as Yup from "yup"
+
+const countryEditSchema = Yup.object({
+    name: Yup.string()
+        .max(256, 'Maximum length of 256 characters.')
+        .required('Required.'),
+    description: Yup.string()
+        .max(256, 'Maximum length of 256 characters.')
+        .required('Required.'),
+    ruler: Yup.string()
+        .max(256, 'Maximum length of 256 characters.')
+        .required('Required.'),
+    founded: Yup.string()
+        .max(256, 'Maximum length of 256 characters.')
+        .required('Required.'),
+})
 
 const initialCountry = {
     campaign_id: "",
@@ -60,22 +76,43 @@ const CountryEdit = () => {
                 ruler: country.ruler,
                 description: country.description,
             }}
+            validationSchema={countryEditSchema}
             onSubmit={(values, { setSubmitting, resetForm }) => {
                 editCountry(values)
                 setSubmitting(false)
                 resetForm()
             }}
         >
-            {({ isSubmitting }) => (
+            {({ errors, touched, isSubmitting }) => (
                 <Form>
                     <label>Country's Name:</label>
                     <Field type="text" name="name" />
+
+                    {errors.name && touched.name ? (
+                        <div className="error">{errors.name}</div>
+                    ) : null}
+
+
                     <label>Country's Ruler:</label>
                     <Field type="text" name="ruler" />
+
+                    {errors.ruler && touched.ruler ? (
+                        <div className="error">{errors.ruler}</div>
+                    ) : null}
+
                     <label>Founded</label>
                     <Field type="text" name="founded" />
+
+                    {errors.founded && touched.founded ? (
+                        <div className="error">{errors.founded}</div>
+                    ) : null}
+
                     <label>Description:</label>
                     <Field type="text" name="description" />
+
+                    {errors.description && touched.description ? (
+                        <div className="error">{errors.description}</div>
+                    ) : null}
 
                     <button type="submit" disabled={isSubmitting}>
                         Submit

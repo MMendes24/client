ade879d6b1d6766980b566b6c5e756a7d056ece2
MMendes24 <mmbusiness1@protonmail.com>
2020-11-18 19:13:28 -0600

    Nav bar styled

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,29 @@
 import React from "react"
 import { useHistory } from "react-router-dom"
+
+// styling
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 import Link from '@material-ui/core/Link';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& > *': {
+            margin: theme.spacing(1),
+        },
+    },
+    navLink: {
+        fontSize: 18,
+        margin: theme.spacing(1),
+    }
+}));
 
 const NavBar = () => {
     const id = localStorage.getItem("user")
     const history = useHistory()
+
+    const classes = useStyles()
 
     const logOut = () => {
         localStorage.clear()
@@ -12,11 +31,18 @@ const NavBar = () => {
     }
 
     return (
-        <div>
-            <h2>Campaign Journal</h2>
+        <Grid
+        className={classes.root}
+        container
+        direction="row"
+        justify="flex-start`"
+        alignItems="center"
+        >
+            <Typography variant="h2" gutterBottom>Campaign Journal</Typography>
             <nav>
 
                 <Link
+                    className={classes.navLink}
                     component="button"
                     variant="button"
                     onClick={() => {
@@ -27,6 +53,7 @@ const NavBar = () => {
                 </Link>
 
                 <Link
+                    className={classes.navLink}
                     component="button"
                     variant="button"
                     onClick={logOut}
@@ -34,8 +61,7 @@ const NavBar = () => {
                     Log Out
                 </Link>
             </nav>
-
-        </div>
+        </Grid>
     )
 }
 
